abcb9bc8517cd5c01af5cfcda699a0525cd7b513
Hadzimgth <133831027+Hadzimgth@users.noreply.github.com>
2025-10-26 02:41:19 +0800

    Add project files.

---
+++ SetTheDate/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SetTheDate.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
